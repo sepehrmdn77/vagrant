ddf139614c1169320209add1026cf9afb1d6ef26
Sepehr <sepehrmaadani98@gmail.com>
2025-02-12 17:38:02 +0200

    New features added to CLI and search has a new function named check

--- Vagrant_CLI.py
+++ Vagrant_CLI.py
@@ -8,13 +8,13 @@ def CLI():
 @CLI.command()
 @click.argument('name')
 def init(name):
-    '''Creating a vagrant machine from vagrant cloud'''
+    '''Enter a name for creating a vagrant machine from vagrant cloud'''
     subprocess.run(f'vagrant init {name}', shell=True)
 
 @CLI.command()
 @click.argument('name')
 def up(name):
-    '''Running the vagrant machine'''
+    '''Enter the name for running the vagrant machine'''
     if search.search(name) == True:
         subprocess.run(f'vagrant up {name}', shell=True)
     else:
@@ -23,7 +23,7 @@ def up(name):
 @CLI.command()
 @click.argument('name')
 def halt(name):
-    '''Stopping the vagrant machine'''
+    '''Enter the name for stopping the vagrant machine'''
     if search.search(name) == True:
         subprocess.run(f'vagrant halt {name}', shell=True)
     else:
@@ -32,7 +32,7 @@ def halt(name):
 @CLI.command()
 @click.argument('name')
 def suspend(name):
-    '''Suspending the vagrant machine'''
+    '''Enter the name for suspending the vagrant machine'''
     if search.search(name) == True:
         subprocess.run(f'vagrant suspend {name}', shell=True)
     else:
@@ -41,11 +41,19 @@ def suspend(name):
 @CLI.command()
 @click.argument('name')
 def resume(name):
-    '''Resuming the vagrant machine'''
+    '''Enter the name for resuming the vagrant machine'''
     if search.search(name) == True:
         subprocess.run(f'vagrant halt {name}', shell=True)
     else:
         click.echo('There is no machine named: {name}!')
+@CLI.command()
+def list():
+    '''Listing all machines'''
+    click.echo(search.check())
+@CLI.command()
+def status():
+    '''Status of all machines'''
+    subprocess.run(f'vagrant status', shell=True)
 
 if __name__ == '__main__':
     CLI()
--- search.py
+++ search.py
@@ -2,3 +2,12 @@ import subprocess
 def search(name):
     is_here = subprocess.check_output("vagrant status", shell=True, text=True).split()
     return name in is_here
+
+def check():
+    mutual_list = ['saved','running','Current', 'machine', 'states:', 'poweroff', '(virtualbox)', 'poweroff', '(virtualbox)', 'This', 'environment', 'represents', 'multiple', 'VMs.', 'The', 'VMs', 'are', 'all', 'listed', 'above', 'with', 'their', 'current', 'state.', 'For', 'more', 'information', 'about', 'a', 'specific', 'VM,', 'run', '`vagrant', 'status', 'NAME`.']
+    checking = subprocess.check_output("vagrant status", shell=True, text=True).split()
+    result = ''
+    for item in checking:
+        if item not in mutual_list:
+            result += f'\n{item}\n'
+    return result
